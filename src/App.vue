f7b9f859be0ffca1a1d24d9beb2bc786fc2704ec
kds <kdskoudous98@gmail.com>
2023-02-26 15:35:36 +0100

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import View from './components/View.vue';
+</script>
+
+<template>
+  
+  <h2>Hello World</h2>
+  <View element="div">
+    Hello from view
+  </View>
+  
+</template>
+
+<style scoped>
+  #app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  font-weight: normal;
+}
+</style>
